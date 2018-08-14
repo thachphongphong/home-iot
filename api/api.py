f95c7f1e822d3e358c00d642ce7a699b28b98c2a
Linh Do <thachphongphong@gmail.com>
2018-08-14 14:39:34 +0700

    Use blue sprint

--- api/api.py
+++ api/api.py
@@ -1,8 +1,9 @@
-from flask import Flask
-import json
 import datetime
+import json
+from flask import Blueprint
 
-app = Flask(__name__)
+api = Blueprint(name='api', import_name=__name__, url_prefix='/api')
+
 light_status = 'off'
 valve_status = 'off'
 fan_status = 'off'
@@ -43,28 +44,23 @@ def setMode(v):
 def warteringTime():
     if (mode == 'AUTO'):
         print("Wartering at %s" % (timestamp))
-        return datetime.time(hour=6, minute=0) <= timestamp <= datetime.time(hour=6, minute=10) or \
-               datetime.time(hour=19, minute=0) <= timestamp <= datetime.time(hour=19, minute=10)
+        return datetime.time(hour=11, minute=20) <= timestamp <= datetime.time(hour=11, minute=30) or \
+               datetime.time(hour=20, minute=0) <= timestamp <= datetime.time(hour=20, minute=10)
     else:
         return False
 
-
-@app.route('/')
-def start():
-    return 'IOT HOME PROJECT!'
-
-@app.route('/api/v1.0/', methods=['GET'])
-def api():
+@api.route('/v1.0/', methods=['GET'])
+def status():
     return 'working'
 
-@app.route('/api/v1.0/light', methods=['GET'])
+@api.route('/v1.0/light', methods=['GET'])
 def get_light_status():
     light_status = getStatus('light')
     if light_status is None:
         setStatus('light', 'off')
     return getStatus('light')
 
-@app.route('/api/v1.0/light', methods=['POST'])
+@api.route('/v1.0/light', methods=['POST'])
 def post_light_status():
     light_status = getStatus('light')
     if light_status == "on":
@@ -73,7 +69,7 @@ def post_light_status():
         setStatus('light', 'on')
     return getStatus('light')
 
-@app.route('/api/v1.0/valve', methods=['GET'])
+@api.route('/v1.0/valve', methods=['GET'])
 def get_valve_status():
     if warteringTime():
         valve_status = 'on'
@@ -83,7 +79,7 @@ def get_valve_status():
         setStatus('valve', 'off')
     return getStatus('valve')
 
-@app.route('/api/v1.0/valve', methods=['POST'])
+@api.route('/v1.0/valve', methods=['POST'])
 def post_valve_status():
     valve_status = getStatus('valve')
     if valve_status == "on":
@@ -92,14 +88,14 @@ def post_valve_status():
         setStatus('valve', 'on')
     return getStatus('valve')
 
-@app.route('/api/v1.0/fan', methods=['GET'])
+@api.route('/v1.0/fan', methods=['GET'])
 def get_fan_status():
     fan_status = getStatus('fan')
     if fan_status is None:
         setStatus('fan', 'off')
     return getStatus('fan')
 
-@app.route('/api/v1.0/fan', methods=['POST'])
+@api.route('/v1.0/fan', methods=['POST'])
 def post_fan_status():
     fan_status = getStatus('fan')
     if fan_status == "on":
@@ -108,14 +104,14 @@ def post_fan_status():
         setStatus('fan', 'on')
     return getStatus('fan')
 
-@app.route('/api/v1.0/status', methods=['GET'])
+@api.route('/v1.0/status', methods=['GET'])
 def get_all_status():
     valve_status = getStatus('valve')
     light_status = getStatus('light')
     fan_status = getStatus('fan')
     return json.dumps({'valve': valve_status, 'light':light_status,'fan':  fan_status})
 
-@app.route('/api/v1.0/mode', methods=['POST'])
+@api.route('/v1.0/mode', methods=['POST'])
 def post_mode():
     if getMode() == 'AUTO':
         setMode('MANUAL')
@@ -123,13 +119,9 @@ def post_mode():
         setMode('AUTO')
     return mode
 
-@app.route('/api/v1.0/mode', methods=['GET'])
+@api.route('/v1.0/mode', methods=['GET'])
 def get_mode():
     mode = getMode()
     if mode is None:
         setMode('MANUAL')
     return getMode()
-
-if __name__ == '__main__':
-
-    app.run("0.0.0.0")
--- app.py
+++ app.py
@@ -9,12 +9,18 @@ from flask_mqtt import Mqtt
 from flask_socketio import SocketIO
 from flask_bootstrap import Bootstrap
 
+# Blueprints
+from api.api import api
+
 eventlet.monkey_patch()
 
 app = Flask(__name__)
-app.config['SECRET'] = 'my secret key'
+
+app.register_blueprint(api)
+
+#app.config['SECRET'] = 'my secret key'
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config['MQTT_BROKER_URL'] = 'localhost'
+app.config['MQTT_BROKER_URL'] = '192.168.77.150'
 app.config['MQTT_BROKER_PORT'] = 1883
 app.config['MQTT_CLIENT_ID'] = 'flask_mqtt'
 app.config['MQTT_USERNAME'] = ''
@@ -22,19 +28,20 @@ app.config['MQTT_PASSWORD'] = ''
 app.config['MQTT_KEEPALIVE'] = 5
 app.config['MQTT_TLS_ENABLED'] = False
 
-
-# Parameters for SSL enabled
-# app.config['MQTT_BROKER_PORT'] = 8883
-# app.config['MQTT_TLS_ENABLED'] = True
-# app.config['MQTT_TLS_INSECURE'] = True
-# app.config['MQTT_TLS_CA_CERTS'] = 'ca.crt'
+topics= ["sonoff1", "sonoff2"]
 
 mqtt = Mqtt(app)
 socketio = SocketIO(app)
 bootstrap = Bootstrap(app)
 
 
-@app.route('/')
+def checkTopic(t):
+    if t in topics:
+        return True
+    else:
+        return False
+
+@app.route('/iot')
 def index():
     return render_template('index.html')
 
@@ -42,13 +49,15 @@ def index():
 @socketio.on('publish')
 def handle_publish(json_str):
     data = json.loads(json_str)
-    mqtt.publish(data['topic'], data['message'], data['qos'])
+    if checkTopic(data['topic']):
+        mqtt.publish(data['topic'], data['message'], data['qos'])
 
 
 @socketio.on('subscribe')
 def handle_subscribe(json_str):
     data = json.loads(json_str)
-    mqtt.subscribe(data['topic'], data['qos'])
+    if checkTopic(data['topic']):
+        mqtt.subscribe(data['topic'], data['qos'])
 
 
 @mqtt.on_message()
